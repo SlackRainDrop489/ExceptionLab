63b132f3d8407817932cfb64d2c6a27d7e34750c
SlackRainDrop489 <quinn967056@my.yosemite.edu>
2025-10-30 09:49:59 -0700

    working on file utils

--- FileUtils.cpp
+++ FileUtils.cpp
@@ -26,30 +26,27 @@ using namespace std;
  * @param file The ifstream object to be opened (passed by reference).
  * @param initialFilename The first filename to try.
  */
-void openFileForReading(std::ifstream& file, std::string initialFilename) {
+void openFileForReading(std::ifstream &file, std::string initialFilename) {
+	// STUDENT TODO: Replace the following 'if' block with a
+	// try...catch block that throws and catches a FileOpenException.
+	try {
+		file.open(initialFilename);
+	} catch (const FileOpenException &e) {
+		cerr << "Error: File could not be opened: " << initialFilename << endl;
 
-    // STUDENT TODO: Replace the following 'if' block with a
-    // try...catch block that throws and catches a FileOpenException.
+		// This logic should be inside your 'catch' block
+		string alternativeFilename;
+		cout << "Enter alternative filename: ";
+		cin >> alternativeFilename;
+		file.open(alternativeFilename);
 
-    file.open(initialFilename);
-    if (!file.is_open()) {
-        cerr << "Error: File could not be opened: " << initialFilename << endl;
-
-        // This logic should be inside your 'catch' block
-        string alternativeFilename;
-        cout << "Enter alternative filename: ";
-        cin >> alternativeFilename;
-        file.open(alternativeFilename);
-
-        if (!file.is_open()) {
-            cerr << "Error: Failed to open alternative file. Exiting." << endl;
-            // In a real program, you might throw a PersistentFileError here,
-            // but for this lab, we will just exit.
-            exit(1); // Exit the program with an error code
-        }
-        cout << "Alternative file opened successfully: " << alternativeFilename << endl;
-    } else {
-        cout << "File opened successfully: " << initialFilename << endl;
-    }
+		if (!file.is_open()) {
+			cerr << "Error: Failed to open alternative file. Exiting." << endl;
+			// In a real program, you might throw a PersistentFileError here,
+			// but for this lab, we will just exit.
+			exit(1); // Exit the program with an error code
+		}
+		cout << "Alternative file opened successfully: " << alternativeFilename << endl;
+	}
+	cout << "File opened successfully: " << initialFilename << endl;
 }
-
